c9a4671e4a0f2912a60c50ec33fd6824b2944b19
Abigail <asyelsub@gmail.com>
2020-10-06 17:58:58 -0400

    our boy now lays eggs

--- Assets/Scripts/FlowerSpawn.cs
+++ Assets/Scripts/FlowerSpawn.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
+//The intent of this script is to spawn eyeballs behind the dude who is moving.
 public class FlowerSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject flowerPrefab;
+
+
+    private float flowerSpacer;
+    private float randomMax;
+
+    private Vector3 flowerPos;
+    private Vector3 flowerRot;
+    private void Start()
     {
-        
+        flowerSpacer = 0;
+        randomMax = UnityEngine.Random.Range(2, 4);
+        flowerPos = new Vector3(0,0,0);
+        //can maybe randomize the x and y as well a bit
+        flowerRot = new Vector3(0,0,90);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (flowerSpacer >= randomMax)
+        {
+            flowerPos = gameObject.transform.position;
+            flowerSpacer = 0f;
+            Instantiate(flowerPrefab, flowerPos, Quaternion.Euler(flowerRot));
+        }
+        else
+        {
+            flowerSpacer += Time.deltaTime;
+        }
     }
 }
