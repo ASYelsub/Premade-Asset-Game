ec62d06fcfb8c60fb4f3ea5e16c20d21731eaee1
Abigail <asyelsub@gmail.com>
2020-10-07 23:58:29 -0400

    f this

--- Assets/Scripts/FlowerPickUp.cs
+++ Assets/Scripts/FlowerPickUp.cs
@@ -15,36 +15,48 @@ public class FlowerPickUp : MonoBehaviour
     private RaycastHit rch;
     public Camera camera;
 
+    private bool raySend;
     private void Start()
     {
         //camera = gameObject.;
+         raySend = false;
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-        /*    //print("MOUSE PLS");
+             raySend = true;
+            //print("MOUSE PLS");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (raySend)
+        {
             Ray rc = new Ray(camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)), camera.transform.forward);
-            Debug.DrawRay(rc.origin, rc.direction, Color.red);
+            
             //Physics.Raycast(new Vector3(transform.position.x, transform.position.y - downAim, transform.position.z), transform.forward, out hit, Mathf.Infinity, layerMask);
             if (Physics.Raycast(rc.origin, rc.direction, out rch, Mathf.Infinity))
             {
+                Debug.DrawRay(rc.origin, rc.direction, Color.red,Mathf.Infinity);
+                print(rc.origin + rc.direction);
                 print("ray sent out");
                 if (rch.collider.gameObject.CompareTag("Flower"))
                 {
-                    activeFlower = Hit.collider.gameObject;
+                    activeFlower = rch.collider.gameObject;
                     print("object hit!");
                 }
                 print(rch.collider.gameObject);
 
-            } */
+            }
+            raySend = false;
         }
 
-        
     }
-    
-    
+
+
     /*if ( Input.GetMouseButtonDown (0)){ 
         RaycastHit hit; 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
